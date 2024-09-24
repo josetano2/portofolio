6460a9a027ebacd9b0436e8cbb4baf9741618701
josetano2 <132138270+josetano2@users.noreply.github.com>
2024-09-25 00:15:06 +0700

    feats: cards

--- src/lib/contents/project-contents.tsx
+++ src/lib/contents/project-contents.tsx
@@ -1,8 +1,8 @@
 import type { IProject, IStack } from "@/lib/models/model";
 import { DiPostgresql, DiReact } from "react-icons/di";
 import { FaGolang  } from "react-icons/fa6";
-import traveloHIThumbnail from "@/lib/images/traveloHI/unknown.png";
-import easyWorkThumbnail from "@/lib/images/EasyWork/S__14262274.jpg"
+import traveloHIThumbnail from "@/lib/images/traveloHI/unknown.jpg";
+import easyWorkThumbnail from "@/lib/images/EasyWork/unknown.jpg"
 
 export const stackContents: IStack[] = [
   {
