1d1a680f3868abe01f488ff97680919e328959c4
josetano2 <josejonathan.tano@gmail.com>
2024-10-08 14:07:53 +0700

    feat: analytics added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
+
+
+import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx(), icon()]
+  integrations: [tailwind(), react(), mdx(), icon()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
