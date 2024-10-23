95d98297bdf6b2678c5bd42c318172a359a1bdc5
josetano2 <josejonathan.tano@gmail.com>
2024-10-23 09:45:37 +0700

    fix: details

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,15 +7,13 @@ import mdx from "@astrojs/mdx";
 
 import icon from "astro-icon";
 
-
 import vercel from "@astrojs/vercel/serverless";
-
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx(), icon()],
   output: "server",
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: { enabled: true },
   }),
 });
